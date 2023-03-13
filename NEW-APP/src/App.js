98b6da02298bfed5e54e777aa749fac127701243
Hoang Nguyen <hoangisnot@gmail.com>
2023-03-13 15:44:21 -0700

    add components files
    also install axios

--- NEW-APP/src/App.js
+++ NEW-APP/src/App.js
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
-// import Field from "./components/field";
-// import Translate from "./components/translate";
-// import Languages from "./components/languages";
+import Field from "./components/field";
+import Translate from "./components/translate";
+import Languages from "./components/languages";
 
 const App = () => {
+  const [language, setLanguage] = useState('es');
+  const [text, setText] = useState('')
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Field />
+      <Languages />
+      <hr />
+      <Translate />
     </div>
   );
 }
