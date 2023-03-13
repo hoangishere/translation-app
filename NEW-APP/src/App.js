67ec83a20cabf39da7c7840403c58b9e4a784415
Hoang Nguyen <hoangisnot@gmail.com>
2023-03-13 15:28:33 -0700

    create react app
    also added files to get ready for development

---
+++ NEW-APP/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+// import Field from "./components/field";
+// import Translate from "./components/translate";
+// import Languages from "./components/languages";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
